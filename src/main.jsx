a604e46a76ccf429e3ec0615f420872b2770b9d9
Murilo Kauan Socek <socekmurilo@gmail.com>
2024-04-23 10:11:01 -0300

    add SpeedInsights

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from './App.jsx'
 import './index.css'
 import {Home} from './Screens/home/Home.jsx'
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
 
 
 
@@ -29,6 +31,7 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Analytics/>
+    <SpeedInsights/>
       <RouterProvider router={router} />
   </React.StrictMode>,
 )
