84a0184c1f4fc68e7d3b396d75b4fec75a1589b7
Renato Mendes <rnt.mendes11@gmail.com>
2024-04-29 10:12:01 -0300

    Screen Payment

--- src/Screens/payment/Payment.jsx
+++ src/Screens/payment/Payment.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import style from "./Payment.module.css";
 import person from '../../assets/pay/person.png';
 import cart from '../../assets/pay/cart.png';
@@ -6,9 +7,15 @@ import ok from '../../assets/pay/ok.png';
 import { NavBar } from "../../components/navBar/NavBar";
 
 export const Payment = () => {
-    return(
+    const [selectedPayment, setSelectedPayment] = useState(null);
+
+    const handlePaymentSelection = (paymentType) => {
+        setSelectedPayment(paymentType);
+    };
+
+    return (
         <>
-            <NavBar/>
+            <NavBar />
             <div className={style["centralizar"]}>
                 <div className={style["sizeWhite"]}>
                     <div className={style["card"]}>
@@ -19,7 +26,51 @@ export const Payment = () => {
                         <img src={creditCard} alt="CreditCard" />
                         <div className={style["bar"]}></div>
                         <img src={ok} alt="OK" />
-                        <h1 className={style["title"]}>FORMA DE PAGAMENTO</h1>
+                        <div className={style["paymentTypes"]}>
+                            <h1>FORMA DE PAGAMENTO</h1> 
+                            <div className={style["paymentType"]}>
+                                <label htmlFor="cartao" className={style["paymentLabel"]}>
+                                    <input
+                                        type="checkbox"
+                                        id="cartao"
+                                        checked={selectedPayment === 'cartao'}
+                                        onChange={() => handlePaymentSelection('cartao')}
+                                        style={{ display: 'none' }}
+                                    />
+                                    <div className={`${style["paymentCheck"]} ${selectedPayment === 'cartao' ? style["checked"] : ""}`}></div> {/* Div representando o estado do checkbox */}
+                                    <h2 className={style["paymentTitle"]}>Cartão</h2>
+                                </label>
+                                <p className={style["paymentDescription"]}>Parcelamento de até 10X sem juros.</p>
+                            </div>
+                            <div className={style["paymentType"]}>
+                                <label htmlFor="pix" className={style["paymentLabel"]}>
+                                    <input
+                                        type="checkbox"
+                                        id="pix"
+                                        checked={selectedPayment === 'pix'}
+                                        onChange={() => handlePaymentSelection('pix')}
+                                        style={{ display: 'none' }}
+                                    />
+                                    <div className={`${style["paymentCheck"]} ${selectedPayment === 'pix' ? style["checked"] : ""}`}></div>
+                                    <h2 className={style["paymentTitle"]}>Pix</h2>
+                                </label>
+                                <p className={style["paymentDescription"]}>O Código Pix gerado para pagamento é válido por 30 minutos após a finalização do pedido.</p>
+                            </div>
+                            <div className={style["paymentType"]}>
+                                <label htmlFor="boleto" className={style["paymentLabel"]}>
+                                    <input
+                                        type="checkbox"
+                                        id="boleto"
+                                        checked={selectedPayment === 'boleto'}
+                                        onChange={() => handlePaymentSelection('boleto')}
+                                        style={{ display: 'none' }}
+                                    />
+                                    <div className={`${style["paymentCheck"]} ${selectedPayment === 'boleto' ? style["checked"] : ""}`}></div>
+                                    <h2 className={style["paymentTitle"]}>Boleto</h2>
+                                </label>
+                                <p className={style["paymentDescription"]}>Vencimento em 1 dia útil. A data de entrega será alterada devido ao tempo de processamento do boleto.</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
